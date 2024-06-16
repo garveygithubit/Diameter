105f83dc2605b8081a7faf99ff7e03207eff528c
Emmanuel <eamensah085@st.ug.edu.gh>
2024-06-16 23:56:03 +0000

    first commit

---
+++ diameter.js
@@ -0,0 +1,9 @@
+var charater = ' mario';
+var age = 30;
+var isBlackBelt = false;
+charater = ' luigi';
+age = 23;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7.5));
---
+++ diameter.ts
@@ -0,0 +1,13 @@
+let charater = ' mario';
+let  age = 30;
+let isBlackBelt = false;
+
+charater = ' luigi'
+
+age = 23;
+
+const circ = (diameter: number) =>{
+    return diameter * Math.PI
+}
+
+console.log(circ(7.5))
